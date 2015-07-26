df952b43645bca5f293e3541262ebcd11abee2e1
ALTree <alb.donizetti@gmail.com>
2015-07-26 16:31:58 +0200

    Refactored files

---
+++ constants_test.go
@@ -0,0 +1,3 @@
+package floatutils
+
+// TODO: implement constants_test
--- misc.go
+++ misc.go
@@ -3,42 +3,36 @@ package floatutils
 import (
 	"math"
 	"math/big"
+	"strconv"
+	"testing"
 )
 
-// Pi returns pi to prec bits of precision
-func Pi(prec uint) *big.Float {
+// returns true if |a - b| < limit, where
+// limit = 0.00 ... 001 having lim precision,
+// scaled by the magnitude of a
+func compareFloats(a, b *big.Float, lim uint, t *testing.T) bool {
 
-	precExt := prec + 64
+	limit := new(big.Float).SetPrec(lim)
 
-	half := new(big.Float).SetPrec(precExt).SetFloat64(0.5)
-	one := new(big.Float).SetPrec(precExt).SetInt64(1)
-	two := new(big.Float).SetPrec(precExt).SetInt64(2)
-	four := new(big.Float).SetPrec(precExt).SetInt64(4)
-	temp := new(big.Float)
+	decimal_lim := int(float64(lim)*math.Log10(2)) - 1
+	limit.Parse("1e-"+strconv.Itoa(decimal_lim), 10)
 
-	a := new(big.Float).SetPrec(precExt).SetInt64(1)      // a_0 = 1
-	b := new(big.Float).Quo(one, Sqrt(two))               // b_0 = 1 / sqrt(2)
-	t := new(big.Float).SetPrec(precExt).SetFloat64(0.25) // t_0 = 1 / 4
-	p := new(big.Float).SetPrec(precExt).SetInt64(1)      // p_0 = 1
+	sub := new(big.Float).SetPrec(lim)
+	sub.Sub(a, b)
 
-	steps := math.Log2(float64(precExt))
+	// scale limit
+	limit.SetMantExp(limit, a.MantExp(nil))
 
-	a2 := new(big.Float)
-	for i := 0; i < int(steps); i++ {
-		a2.Add(a, b).Mul(a2, half) // a_{n+1} = (a_{n} + b_{n}) / 2
-		b = Sqrt(temp.Mul(a, b))   // b_{n+1} = sqrt(a_{n} * b_{n})
-
-		temp.Sub(a, a2).Mul(temp, temp) // temp = (a_{n} - a_{n+1})²
-		t.Sub(t, temp.Mul(p, temp))     // t_{n+1} = t_{n} - p_{n} * temp
-
-		p.Mul(two, p) // p_{n+1} = 2 * p_{n}
-
-		a.Copy(a2)
+	if sub.Abs(sub).Cmp(limit) > 0 {
+		t.Errorf("limit = %.100f\n", limit)
+		t.Errorf("sub   = %.100f\n", sub)
+		return false
 	}
 
-	temp.Add(a, b)
-	temp.Mul(temp, temp)
-	res := new(big.Float).Quo(temp, t.Mul(four, t)) // pi = (a_{n+1} + b_{n+1})² / (4 * t_{n+1})
-
-	return res.SetPrec(prec)
+	return true
 }
+
+// ---------- Benchmarks ----------
+
+// global benchmark dummy variable
+var result *big.Float
