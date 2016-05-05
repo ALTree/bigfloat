a997bb899fcf27aa10cd68d5c00c306df8bc9454
Alberto Donizetti <alb.donizetti@gmail.com>
2016-05-05 18:25:56 +0200

    exp.go: finish exp implementation via newton
    Also reuse guess in newton
    
    AgmPrec53-4           27.2µs ± 1%    26.6µs ± 0%   -2.09%          (p=0.008 n=5+5)
    AgmPrec100-4          25.9µs ± 0%    25.4µs ± 1%   -1.90%          (p=0.008 n=5+5)
    AgmPrec1000-4          108µs ± 1%     107µs ± 1%   -1.03%          (p=0.016 n=5+5)
    AgmPrec10000-4        2.17ms ± 0%    2.17ms ± 0%     ~             (p=0.690 n=5+5)
    AgmPrec100000-4       76.8ms ± 0%    76.8ms ± 0%     ~             (p=1.000 n=5+5)
    
    PiPrec53-4            42.8µs ± 4%    40.8µs ± 0%   -4.80%          (p=0.008 n=5+5)
    PiPrec100-4           38.3µs ± 0%    38.3µs ± 4%     ~             (p=0.690 n=5+5)
    PiPrec1000-4           147µs ± 1%     145µs ± 1%   -1.21%          (p=0.016 n=5+5)
    PiPrec10000-4         2.77ms ± 1%    2.77ms ± 2%     ~             (p=0.690 n=5+5)
    PiPrec100000-4         102ms ± 1%     102ms ± 1%     ~             (p=0.151 n=5+5)
    
    Exp2Prec53-4           149µs ± 1%      71µs ± 1%  -52.26%          (p=0.008 n=5+5)
    Exp2Prec100-4          238µs ± 0%     157µs ± 0%  -34.30%          (p=0.008 n=5+5)
    Exp2Prec1000-4         766µs ± 0%    1116µs ± 0%  +45.65%          (p=0.008 n=5+5)
    Exp2Prec10000-4       18.6ms ± 0%    15.0ms ± 1%  -19.11%          (p=0.016 n=4+5)
    Exp2Prec100000-4       530ms ± 0%     432ms ± 0%  -18.46%          (p=0.008 n=5+5)
    
    Log2Prec53-4          70.3µs ± 1%    66.7µs ± 0%   -5.04%          (p=0.008 n=5+5)
    Log2Prec100-4         82.5µs ± 1%    80.4µs ± 1%   -2.55%          (p=0.008 n=5+5)
    Log2Prec1000-4         232µs ± 3%     224µs ± 0%   -3.62%          (p=0.008 n=5+5)
    Log2Prec10000-4       3.79ms ± 0%    3.86ms ± 3%     ~             (p=0.421 n=5+5)
    Log2Prec100000-4       135ms ± 0%     135ms ± 0%   -0.48%          (p=0.008 n=5+5)
    
    PowIntPrec53-4        1.03µs ± 1%    1.02µs ± 3%     ~             (p=0.841 n=5+5)
    PowIntPrec100-4       1.42µs ± 0%    1.44µs ± 0%   +0.95%          (p=0.016 n=5+4)
    PowIntPrec1000-4      2.01µs ± 1%    2.02µs ± 1%     ~             (p=0.175 n=5+5)
    PowIntPrec10000-4     2.29µs ± 0%    2.30µs ± 0%   +0.37%          (p=0.016 n=5+4)
    PowIntPrec100000-4    2.32µs ± 0%    2.32µs ± 0%     ~             (p=0.603 n=5+5)
    PowPrec53-4            361µs ± 1%     392µs ± 1%   +8.72%          (p=0.008 n=5+5)
    PowPrec100-4           506µs ± 0%     394µs ± 1%  -22.09%          (p=0.008 n=5+5)
    PowPrec1000-4         1.63ms ± 0%    1.44ms ± 1%  -11.57%          (p=0.008 n=5+5)
    PowPrec10000-4        24.1ms ± 0%    20.3ms ± 0%  -15.48%          (p=0.008 n=5+5)
    PowPrec100000-4        712ms ± 4%     618ms ± 3%  -13.15%          (p=0.008 n=5+5)
    
    Sqrt2Prec53-4         1.93µs ± 0%    1.83µs ± 1%   -5.24%          (p=0.008 n=5+5)
    Sqrt2Prec100-4        3.45µs ± 1%    3.31µs ± 1%   -4.00%          (p=0.008 n=5+5)
    Sqrt2Prec1000-4       8.77µs ± 1%    8.65µs ± 0%   -1.35%          (p=0.008 n=5+5)
    Sqrt2Prec10000-4      54.8µs ± 1%    54.1µs ± 0%   -1.29%          (p=0.008 n=5+5)
    Sqrt2Prec100000-4     1.01ms ± 2%    1.01ms ± 2%     ~             (p=0.841 n=5+5)
    
    There's a strange regression in Exp2Prec1000, needs investigation
    
    Fixes #21

--- exp.go
+++ exp.go
@@ -30,19 +30,18 @@ func Exp(z *big.Float) *big.Float {
 	// try to get initial estimate using IEEE-754 math
 	zf, _ := z.Float64()
 	if zfs := math.Exp(zf); zfs == math.Inf(+1) || zfs == 0 {
-		// too big for IEEE-754 math, perform
-		// argument reduction using e^{2z} = (e^z)²
-		x := new(big.Float)
-		halfZ := new(big.Float).Set(z).SetPrec(z.Prec() + 64)
-		halfZ.Quo(z, big.NewFloat(2))
-		halfExp := Exp(halfZ)
-		return x.Mul(halfExp, halfExp).SetPrec(z.Prec())
+		// too big or too small for IEEE-754 math,
+		// perform argument reduction using
+		//     e^{2z} = (e^z)²
+		halfZ := new(big.Float).Mul(z, big.NewFloat(0.5))
+		halfExp := Exp(halfZ.SetPrec(z.Prec() + 64))
+		return new(big.Float).Mul(halfExp, halfExp).SetPrec(z.Prec())
 	} else {
 		// we got a nice IEEE-754 estimate
 		guess.SetFloat64(zfs)
 	}
 
-	// f(t)/f'(t) = t(log(t) - z)
+	// f(t)/f'(t) = t*(log(t) - z)
 	f := func(t *big.Float) *big.Float {
 		x := new(big.Float)
 		x.Sub(Log(t), z)
