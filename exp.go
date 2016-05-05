cc6aff12d96697ce6d3c2b6b238a4ce693876552
Alberto Donizetti <alb.donizetti@gmail.com>
2016-05-05 17:28:49 +0200

    exp.go: switch to general newton function

--- exp.go
+++ exp.go
@@ -25,37 +25,31 @@ func Exp(z *big.Float) *big.Float {
 		return big.NewFloat(0).SetPrec(z.Prec())
 	}
 
-	x := new(big.Float)
+	guess := new(big.Float)
 
 	// try to get initial estimate using IEEE-754 math
 	zf, _ := z.Float64()
 	if zfs := math.Exp(zf); zfs == math.Inf(+1) || zfs == 0 {
 		// too big for IEEE-754 math, perform
-		// argument reduction using e^{2x} = (e^x)²
+		// argument reduction using e^{2z} = (e^z)²
+		x := new(big.Float)
 		halfZ := new(big.Float).Set(z).SetPrec(z.Prec() + 64)
 		halfZ.Quo(z, big.NewFloat(2))
 		halfExp := Exp(halfZ)
 		return x.Mul(halfExp, halfExp).SetPrec(z.Prec())
 	} else {
 		// we got a nice IEEE-754 estimate
-		x.SetFloat64(zfs)
+		guess.SetFloat64(zfs)
 	}
 
-	var prec uint = 32
-
-	t := new(big.Float).SetPrec(prec + 64) // guard digits
-
-	// Solve log(x) - z = 0 for x to find exp(z),
-	// using Newton's method.
-	for prec < 2*z.Prec() {
-		t = Log(x)  // t = log(x_n)
-		t.Sub(t, z) // t = log(x_n) - z
-		t.Mul(t, x) // t = x_n * (log(x_n) - z)
-		x.Sub(x, t) // x_{n+1} = x_n - x_n * (log(x_n) - z)
-		prec *= 2
-		x.SetPrec(prec + 64)
+	// f(t)/f'(t) = t(log(t) - z)
+	f := func(t *big.Float) *big.Float {
+		x := new(big.Float)
+		x.Sub(Log(t), z)
+		return x.Mul(x, t)
 	}
 
-	return x.SetPrec(z.Prec())
+	x := newton2(f, guess, z.Prec())
 
+	return x
 }
